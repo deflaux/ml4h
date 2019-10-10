2f089ccbdef843fb041ccbf992672947093a7987
Paolo Di Achille <paolo.diachille@gmail.com>
2019-10-10 12:52:39 +0000

    Corrected naming of output folder

--- ecg_compare_models_train.sh
+++ ecg_compare_models_train.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 
 TENSORS="/mnt/disks/ecg-rest-31k/2019-06-10/"
-OUTPUT_TENSOR_LIST="ecg_rhythm ecg_coarse ecg_semi_coarse ecg_semi_coarse_with_poor \
-		    ecg_normal ecg_infarct ecg_block acute_mi anterior_blocks av_block\ 
+OUTPUT_TENSOR_LIST="ecg_block acute_mi anterior_blocks av_block\ 
                     fine_rhythms incomplete_right_bundle_branch_block infarcts \
                     left_atrial_enlargement left_ventricular_hypertrophy lvh_fine 
                     prolonged_qt p_axis p_duration" 
 
 for output_tensor in ${OUTPUT_TENSOR_LIST}
 do
+    echo "Training for "${output_tensor} 
     ./scripts/tf.sh /home/${USER}/ml/ml4cvd/recipes.py --mode train \
 		--tensors ${TENSORS} \
 		--input_tensors ecg_rest \
@@ -16,5 +16,5 @@ do
 		--training_steps 96 --validation_steps 32 --test_steps 24 \
 		--batch_size 32 --epochs 64 --patience 12 \
 		--output_folder /home/${USER}/ml/trained_models/ \
-		--id ${USER}_${INPUT_TENSORS}_${output_tensor} --learning_rate 0.00002
+		--id ${USER}__${output_tensor} --learning_rate 0.00002
 done
