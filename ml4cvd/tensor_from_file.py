80e41ad63675086007d5a8c453519fcd964f1f95
Samuel Friedman <lucidtronix@gmail.com>
2019-11-26 16:09:10 -0500

    changes

--- ml4cvd/tensor_from_file.py
+++ ml4cvd/tensor_from_file.py
@@ -518,3 +518,16 @@ TMAPS['mri_slice_blackout_segmented_weighted'] = TensorMap('mri_slice_segmented'
                                                            loss=weighted_crossentropy([0.1, 25.0, 25.0], 'mri_slice_blackout_segmented'))
 TMAPS['mri_slice_blackout'] = TensorMap('mri_slice_blackout', (256, 256, 1), tensor_from_file=mri_slice_blackout_tensor_from_file,
                                         dependent_map=TMAPS['mri_slice_blackout_segmented_weighted'])
+
+
+def _slice_tensor(tensor_key, slice_index):
+    def _slice_tensor_from_file(tm, hd5, dependents={}):
+        tensor = np.zeros(tm.shape, dtype=np.float32)
+        tensor[..., 0] = np.array(hd5[tensor_key][slice_index], dtype=np.float32)
+        return tm.normalize_and_validate(tensor)
+    return _slice_tensor_from_file
+
+
+TMAPS['lax_4ch_diastole_slice'] = TensorMap('lax_4ch_diastole_slice', (256, 256, 1), group='root_array', loss='logcosh',
+                                            tensor_from_file=_slice_tensor('cine_segmented_lax_4ch', 0),
+                                            normalization={'zero_mean_std1': True})
