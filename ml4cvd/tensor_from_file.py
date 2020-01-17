88ecde720fca5038242bf92f7db13f73406e2b6d
ndiamant <ndiamant@broadinstitute.org>
2020-01-17 19:36:08 +0000

    more augmentation

--- ml4cvd/tensor_from_file.py
+++ ml4cvd/tensor_from_file.py
@@ -150,6 +150,9 @@ def _first_date_bike_pretest_augmented(tm: TensorMap, hd5: h5py.File, dependents
     tensor = np.array(hd5[first_date_path][start: start + tm.shape[0]], dtype=np.float32)
     tensor = _fail_nan(tensor)
     tensor += np.random.randn(*tm.shape) * np.random.randint(0, 3)
+    tensor[:, 0] *= .9 + .2 * np.random.rand()
+    tensor[:, 1] *= .9 + .2 * np.random.rand()
+    tensor[:, 2] *= .9 + .2 * np.random.rand()
     return tm.normalize_and_validate(tensor).reshape(tm.shape)
 
 
