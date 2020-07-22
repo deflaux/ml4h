5f00bcce5726fbce78077d65e9510d961c05ecee
Samwell Freeman <lucidtronix@gmail.com>
2020-07-22 12:24:51 -0400

    la vol

--- ml4cvd/tensor_from_file.py
+++ ml4cvd/tensor_from_file.py
@@ -1768,13 +1768,19 @@ TMAPS['cine_lax_4ch_192_16_3_4d'] = TensorMap(
     normalization=ZeroMeanStd1(),
 )
 TMAPS['cine_lvot_208_16_3_4d'] = TensorMap(
-    'cine_segmented_lvot', Interpretation.CONTINUOUS, shape=(208, 192, 16, 1), path_prefix='ukb_cardiac_mri',
+    'cine_segmented_lvot_208_16_3_4d', Interpretation.CONTINUOUS, shape=(208, 192, 16, 1), path_prefix='ukb_cardiac_mri',
     tensor_from_file=_slice_subset_tensor('cine_segmented_lvot', 0, 48, 3, pad_shape=(208, 192, 48, 1)),
     normalization=ZeroMeanStd1(),
 )
-TMAPS['cine_lvot_208_4d'] = TensorMap(
-    'cine_segmented_lvot', Interpretation.CONTINUOUS, shape=(208, 160, 50, 1), path_prefix='ukb_cardiac_mri',
-    tensor_from_file=_pad_crop_tensor, normalization=ZeroMeanStd1(),
+TMAPS['cine_lvot_192_16_3'] = TensorMap(
+    'cine_segmented_lvot_192_16_3', Interpretation.CONTINUOUS, shape=(192, 160, 16), path_prefix='ukb_cardiac_mri',
+    tensor_from_file=_slice_subset_tensor('cine_segmented_lvot', 0, 48, 3, pad_shape=(192, 160, 48)),
+    normalization=ZeroMeanStd1(),
+)
+TMAPS['cine_lvot_192_16_3_4d'] = TensorMap(
+    'cine_segmented_lvot_192_16_3_4d', Interpretation.CONTINUOUS, shape=(192, 160, 16, 1), path_prefix='ukb_cardiac_mri',
+    tensor_from_file=_slice_subset_tensor('cine_segmented_lvot', 0, 48, 3, pad_shape=(192, 160, 48, 1)),
+    normalization=ZeroMeanStd1(),
 )
 
 TMAPS['cine_lax_3ch_192'] = TensorMap(
