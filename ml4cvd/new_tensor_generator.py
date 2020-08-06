cbf1d8b54d6880fc07d42e9ea42746c6156221de
ndiamant <ndiamant@broadinstitute.org>
2020-08-06 16:24:15 -0400

    uses tf.data.Dataset

--- ml4cvd/new_tensor_generator.py
+++ ml4cvd/new_tensor_generator.py
@@ -1,9 +1,12 @@
+import os
 from typing import Set, Any, Dict, List, Callable, Tuple, Optional
+from contextlib import contextmanager, ExitStack
 from abc import ABC, abstractmethod
 import numpy as np
 import h5py
+import tensorflow as tf
 
-from ml4cvd.TensorMap import TensorMap
+from ml4cvd.TensorMap import TensorMap, Interpretation
 
 
 class StateSetter(ABC):
@@ -18,10 +21,6 @@ class StateSetter(ABC):
         pass
 
 
-Batch = Dict[str, np.ndarray]
-BatchFunction = Callable[[Batch, Batch, List[int]], Any]
-
-
 class TensorGetter(ABC):
 
     required_states: Set[str]
@@ -29,30 +28,23 @@ class TensorGetter(ABC):
     shape: Tuple[int, ...]
 
     @abstractmethod
+    @contextmanager
     def get_tensor(self, evaluated_states: Dict[str, Any]) -> np.ndarray:
         pass
 
 
-# TODO: main thread batch, handle errors, different sample_id iterators
-class TensorGenerator:
-
+class SampleGetter:
+    """Gets all of the data for one sample_id"""
     def __init__(
-            self, input_tensor_getters: List[TensorGetter], output_tensor_getters: List[TensorGetter],
-            sample_ids: List[int],
-            batch_size: int, batch_function: BatchFunction,
+            self,
+            input_tensor_getters: List[TensorGetter],
+            output_tensor_getters: List[TensorGetter],
             state_setters: List[StateSetter],
-            return_ids: bool = False,
-            num_workers: int = 0,
     ):
         self.input_tensor_getters = input_tensor_getters
         self.output_tensor_getters = output_tensor_getters
-        self.sample_ids = sample_ids
-        self.batch_size = batch_size
-        self.batch_function = batch_function
-        self.return_ids = return_ids
         self.state_setters = state_setters
         self._check_states()
-        self.num_workers = num_workers
 
     def _check_states(self):
         all_state_names = {state.get_name() for state in self.state_setters}
@@ -63,27 +55,20 @@ class TensorGenerator:
                     f'TensorGetter {getter.name} is missing required StateGetters {missing_states}.'
                 )
 
-    def _evaluate_states(self, sample_id: int):
-        return {
-            state.get_name(): state.get_state(sample_id)
-            for state in self.state_setters
-        }
+    @contextmanager
+    def _evaluate_states(self, sample_id: int) -> Dict[str, Any]:
+        with ExitStack() as stack:
+            yield {
+                state.get_name(): stack.enter_context(state.get_state(sample_id))
+                for state in self.state_setters
+            }
 
-    def get_tensors(self, sample_id: int) -> Tuple[Batch, Batch]:
-        """Get one sample worth of tensors"""
-        evaluated_states = self._evaluate_states(sample_id)
-        sample_in = {}
-        for tensor_getter in self.input_tensor_getters:
-            tensor = tensor_getter.get_tensor(evaluated_states)
-            sample_in[tensor_getter.name] = tensor
-        sample_out = {}
-        for tensor_getter in self.output_tensor_getters:
-            tensor = tensor_getter.get_tensor(evaluated_states)
-            sample_out[tensor_getter.name] = tensor
-        return sample_in, sample_out
-
-    def __next__(self) -> Tuple[Batch, Batch, Optional[List[int]]]:
-        pass  # TODO multiprocess generate batch
+    def __call__(self, sample_id: int) -> List[Tuple[Dict[str, np.ndarray], Dict[str, np.ndarray]]]:
+        with self._evaluate_states(sample_id) as evaluated_states:
+            return [(
+                {tensor_getter.name: tensor_getter.get_tensor(evaluated_states) for tensor_getter in self.input_tensor_getters},
+                {tensor_getter.name: tensor_getter.get_tensor(evaluated_states) for tensor_getter in self.output_tensor_getters},
+            )]
 
 
 class HD5State(StateSetter):
@@ -91,9 +76,10 @@ class HD5State(StateSetter):
     def __init__(self, hd5_paths: Dict[int, str]):
         self.hd5_paths = hd5_paths
 
-    # TODO: should be context_manager?
+    @contextmanager
     def get_state(self, sample_id: int) -> h5py.File:
-        return h5py.File(self.hd5_paths[sample_id], 'r')
+        with h5py.File(self.hd5_paths[sample_id], 'r') as hd5:
+            yield hd5
 
     @staticmethod
     def get_name() -> str:
@@ -102,11 +88,11 @@ class HD5State(StateSetter):
 
 class TensorMapTensorGetter(TensorGetter):
 
-    def __init__(self, tensor_map: TensorMap, augment: bool):
+    def __init__(self, tensor_map: TensorMap, augment: bool, is_input: bool):
         self.required_states = {HD5State.get_name()}
         self.required_state = HD5State.get_name()
         self.tensor_map = tensor_map
-        self.name = tensor_map.name
+        self.name = tensor_map.input_name() if is_input else tensor_map.output_name()
         self.augment = augment
 
     def get_tensor(self, evaluated_states: Dict[str, Any]) -> np.ndarray:
@@ -117,22 +103,57 @@ class TensorMapTensorGetter(TensorGetter):
         return self.tensor_map.postprocess_tensor(tensor, self.augment, hd5)
 
 
+def _hd5_path_to_sample_id(path: str) -> int:
+    return int(os.path.basename(path).replace('.hd5', ''))
+
+
+INTERPRETATION_TO_TENSOR_FLOW_TYPE = {  # TODO: what types?
+    Interpretation.CONTINUOUS: tf.float32,
+    Interpretation.CATEGORICAL: tf.int16,
+    Interpretation.EMBEDDING: tf.float32,
+    Interpretation.LANGUAGE: tf.float32,
+    Interpretation.TIME_TO_EVENT: tf.float64,
+    Interpretation.SURVIVAL_CURVE: tf.float64,
+    Interpretation.DISCRETIZED: tf.int32,
+    Interpretation.MESH: tf.float64,
+}
+
+
+def tensor_maps_to_output_types(tensor_maps_in: List[TensorMap], tensor_maps_out: List[TensorMap]):
+    return (
+        {tmap.input_name(): INTERPRETATION_TO_TENSOR_FLOW_TYPE[tmap.interpretation] for tmap in tensor_maps_in},
+        {tmap.output_name(): INTERPRETATION_TO_TENSOR_FLOW_TYPE[tmap.interpretation] for tmap in tensor_maps_out},
+    )
+
+
+def sample_getter_from_tensor_maps(
+        sample_id_to_path: Dict[int, str],
+        tensor_maps_in: List[TensorMap], tensor_maps_out: List[TensorMap],
+        augment: bool = False,
+) -> SampleGetter:
+    hd5_state = HD5State(sample_id_to_path)
+    input_tensor_getters = [TensorMapTensorGetter(tmap, augment, True) for tmap in tensor_maps_in]
+    output_tensor_getters = [TensorMapTensorGetter(tmap, augment, False) for tmap in tensor_maps_out]
+    return SampleGetter(
+        input_tensor_getters=input_tensor_getters, output_tensor_getters=output_tensor_getters,
+        state_setters=[hd5_state],
+    )
+
+
 def tensor_generator_from_tensor_maps(
         hd5_paths: List[str],
         tensor_maps_in: List[TensorMap], tensor_maps_out: List[TensorMap],
-        batch_size: int, batch_function: BatchFunction,
-        return_ids: bool = False,
-        num_workers: int = 0,
+        batch_size: int,
+        num_workers: int = 0,  # TODO: use this stuff
         augment: bool = False,
-) -> TensorGenerator:
-    """For backwards compatibility."""
-    sample_id_to_path = {i: path for i, path in enumerate(hd5_paths)}
-    hd5_state = HD5State(sample_id_to_path)
-    input_tensor_getters = [TensorMapTensorGetter(tmap, augment) for tmap in tensor_maps_in]
-    output_tensor_getters = [TensorMapTensorGetter(tmap, augment) for tmap in tensor_maps_out]
-    return TensorGenerator(
-        input_tensor_getters=input_tensor_getters, output_tensor_getters=output_tensor_getters,
-        sample_ids=list(sample_id_to_path), state_setters=[hd5_state],
-        batch_size=batch_size, batch_function=batch_function, return_ids=return_ids,
-        num_workers=num_workers,
+) -> tf.data.Dataset:
+    sample_id_to_path = {_hd5_path_to_sample_id(path): path for path in hd5_paths}
+    sample_getter = sample_getter_from_tensor_maps(sample_id_to_path, tensor_maps_in, tensor_maps_out, augment)
+    output_types = tensor_maps_to_output_types(tensor_maps_in, tensor_maps_out)
+    return tf.data.Dataset.from_tensor_slices(  # TODO: This feels overly complicated
+        list(sample_id_to_path.keys())).interleave(
+        lambda sample_id: tf.data.Dataset.from_generator(
+            sample_getter, args=(sample_id,),
+            output_types=output_types,
+        )
     )
--- tests/test_new_tensor_generator.py
+++ tests/test_new_tensor_generator.py
@@ -1,24 +1,39 @@
 import pytest
+import numpy as np
 
-from ml4cvd.new_tensor_generator import tensor_generator_from_tensor_maps
-from ml4cvd.tensor_generators import _identity_batch, BATCH_INPUT_INDEX, BATCH_OUTPUT_INDEX
+from ml4cvd.new_tensor_generator import tensor_generator_from_tensor_maps, _hd5_path_to_sample_id, sample_getter_from_tensor_maps
 from ml4cvd.test_utils import TMAPS_UP_TO_4D
 from ml4cvd.test_utils import build_hdf5s
 
 
-def test_tensor_generator_from_tensor_maps(tmpdir_factory):
+@pytest.fixture(scope='session')
+def expected_tensors(tmpdir_factory):
     temp_dir = tmpdir_factory.mktemp('explore_tensors')
     tmaps = TMAPS_UP_TO_4D
-    expected = build_hdf5s(temp_dir, tmaps, n=pytest.N_TENSORS)
-    paths = [path for path, _ in expected]
+    return build_hdf5s(temp_dir, tmaps, n=pytest.N_TENSORS)
+
+
+def test_tensor_generator_from_tensor_maps(expected_tensors):
+    paths = [path for path, _ in expected_tensors]
     gen = tensor_generator_from_tensor_maps(
         hd5_paths=list(paths),
         tensor_maps_in=TMAPS_UP_TO_4D, tensor_maps_out=TMAPS_UP_TO_4D,
-        batch_size=1, batch_function=_identity_batch, return_ids=True,
-    )
-    sample_id_to_path = gen.state_setters[0].hd5_paths
+        batch_size=1,
+    ).as_numpy_iterator()
+    sample_id_to_path = {_hd5_path_to_sample_id(path): path for path in paths}
+    for (inp, out), sample_id in zip(gen, sample_id_to_path.keys()):
+        path = sample_id_to_path[sample_id]
+        for tmap in TMAPS_UP_TO_4D:
+            assert np.array_equal(expected_tensors[path, tmap], inp[tmap.input_name()])
+            assert np.array_equal(expected_tensors[path, tmap], out[tmap.output_name()])
+
+
+def test_sample_getter_from_tensor_maps(expected_tensors):
+    paths = [path for path, _ in expected_tensors]
+    sample_id_to_path = {_hd5_path_to_sample_id(path): path for path in paths}
     path_to_sample_id = {path: sample_id for sample_id, path in sample_id_to_path.items()}
-    for (path, tm), value in expected.items():
-        fetched = gen.get_tensors(path_to_sample_id[path])
-        assert (fetched[BATCH_INPUT_INDEX][tm.name] == value).all()
-        assert (fetched[BATCH_OUTPUT_INDEX][tm.name] == value).all()
+    getter = sample_getter_from_tensor_maps(sample_id_to_path, TMAPS_UP_TO_4D, TMAPS_UP_TO_4D, False)
+    for (path, tm), value in expected_tensors.items():
+        fetched = getter(path_to_sample_id[path])
+        assert (fetched[0][0][tm.input_name()] == value).all()
+        assert (fetched[0][1][tm.output_name()] == value).all()
